7b0e3fce85e551bd105ab5ef1b234a9334bb2da1
Anum Farghani <127afarghani@frhsd.com>
2026-05-01 12:49:39 -0400

    modul

--- js/script.js
+++ js/script.js
@@ -1,1 +1,1 @@
-alert ('Select between rock, paper, or scissors below...within the starry night. Test your luck!');
+//alert ('Select between rock, paper, or scissors below...within the starry night. Test your luck!');
