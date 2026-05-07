e009edab3971d718e7f749f255a833457c0181e4
Anum Farghani <127afarghani@frhsd.com>
2026-05-07 12:00:01 -0400

    js modal?

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
 // modal
 //alert ('Select between rock, paper, or scissors below...within the starry night. Test your luck!');
+//modal code from GC
+
+document.addEventListener('DOMContentLoaded', function () {
+            var instructionsModal = new bootstrap.Modal(document.getElementById('gameRules'));
+            instructionsModal.show();
+});
