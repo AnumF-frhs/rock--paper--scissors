43ae85e2b6b5393c6fa2dcd45439a7681a51ca5c
Anum Farghani <127afarghani@frhsd.com>
2026-05-14 13:44:26 -0400

    gdgv

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-// modal
-//alert('Do you think you could beat the opponent? Try it out! ');
-//modal code from GC
-/*
-document.addEventListener('DOMContentLoaded', function () {
-            var instructionsModal = new bootstrap.Modal(document.getElementById('gameRules'));
-            instructionsModal.show();
-});
-*/
-
-
-
 //5/12 starting JS from GC, some code is from the Formative!
 let playerScore = 0;
 let computerScore = 0;
